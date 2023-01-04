9a29cec387579e55d57ebb7683fa629e06ef31a7
marcelo_victor <marcelo.victor525@gmail.com>
2023-01-03 23:51:19 -0300

    Adicionado metodo Transferir

--- bytebank/ContaCorrente.cs
+++ bytebank/ContaCorrente.cs
@@ -30,5 +30,19 @@ namespace bytebank
                 return false;
             }
         }
+
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if(this.saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.Sacar (valor);
+                destino.Depositar (valor);
+                return true;
+            }
+        }
     }
 }
--- bytebank/Program.cs
+++ bytebank/Program.cs
@@ -6,6 +6,21 @@ contaDoAndre.numero_agencia = 15;
 contaDoAndre.conta = "1010-x";
 contaDoAndre.saldo = 100;
 
+ContaCorrente contaDaMaria = new ContaCorrente();
+contaDaMaria.titular = "Maria Souza";
+contaDaMaria.numero_agencia = 17;
+contaDaMaria.conta = "1010-5";
+contaDaMaria.saldo = 350;
+
+Console.WriteLine("Saldo da conta d Maria = " + contaDaMaria.saldo);
+
+contaDoAndre.Transferir(50, contaDaMaria);
+Console.WriteLine("Saldo da conta do andré = " + contaDoAndre.saldo);
+Console.WriteLine("Saldo da conta da Maria = " + contaDaMaria.saldo);
+
+
+
+/*
 Console.WriteLine("Saldo da conta do andré = " + contaDoAndre.saldo);
 
 contaDoAndre.Depositar(100);
@@ -19,4 +34,5 @@ if (contaDoAndre.Sacar(100) == true)
 else
 {
     Console.WriteLine("Saldo insuficiente");
-}
+}*/
+
