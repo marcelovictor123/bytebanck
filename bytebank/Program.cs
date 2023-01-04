5ea7d2625571cf90e4551a3099ae22ea9443ec0a
marcelo_victor <marcelo.victor525@gmail.com>
2023-01-03 23:41:43 -0300

    Adicionado metodo Sacar

--- bytebank/Program.cs
+++ bytebank/Program.cs
@@ -11,3 +11,12 @@ Console.WriteLine("Saldo da conta do andré = " + contaDoAndre.saldo);
 contaDoAndre.Depositar(100);
 
 Console.WriteLine("Saldo da conta do andré pós deposito = " + contaDoAndre.saldo);
+
+if (contaDoAndre.Sacar(100) == true)
+{
+    Console.WriteLine("Saldo da conta do andré pós saque = " + contaDoAndre.saldo);
+}
+else
+{
+    Console.WriteLine("Saldo insuficiente");
+}
