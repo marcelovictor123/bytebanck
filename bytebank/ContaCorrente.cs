55b6c92f7ade647a348abefd9ba4d05266f48051
marcelo_victor <marcelo.victor525@gmail.com>
2023-01-05 22:11:01 -0300

    Adicionando referencia

--- bytebank/ContaCorrente.cs
+++ bytebank/ContaCorrente.cs
@@ -11,7 +11,7 @@ namespace bytebank
         public int numero_agencia;
         public string conta;
         public string titular;
-        public double saldo;
+        public double saldo=100;
 
         public void Depositar(double valor)
         {
--- bytebank/Program.cs
+++ bytebank/Program.cs
@@ -1,38 +1,45 @@
-﻿using bytebank;
+﻿//using bytebank;
+
+//ContaCorrente contaDoAndre = new ContaCorrente();
+//contaDoAndre.titular = "André silva";
+//contaDoAndre.numero_agencia = 15;
+//contaDoAndre.conta = "1010-x";
+//contaDoAndre.saldo = 100;
+
+//ContaCorrente contaDaMaria = new ContaCorrente();
+//contaDaMaria.titular = "Maria Souza";
+//contaDaMaria.numero_agencia = 17;
+//contaDaMaria.conta = "1010-5";
+//contaDaMaria.saldo = 350;
+
+//Console.WriteLine("Saldo da conta d Maria = " + contaDaMaria.saldo);
+
+//contaDoAndre.Transferir(50, contaDaMaria);
+//Console.WriteLine("Saldo da conta do andré = " + contaDoAndre.saldo);
+//Console.WriteLine("Saldo da conta da Maria = " + contaDaMaria.saldo);
+
+//ContaCorrente contaDoPedro= new ContaCorrente();
+//Console.WriteLine(contaDoPedro.titular);
+//Console.WriteLine(contaDoPedro.saldo);
+//Console.WriteLine(contaDoPedro.numero_agencia);
+//Console.WriteLine(contaDoPedro.conta);
+
+using bytebank;
 
 ContaCorrente contaDoAndre = new ContaCorrente();
-contaDoAndre.titular = "André silva";
+contaDoAndre.titular = "André Silva";
 contaDoAndre.numero_agencia = 15;
-contaDoAndre.conta = "1010-x";
+contaDoAndre.conta = "1010-X";
 contaDoAndre.saldo = 100;
+Console.WriteLine("Saldo da conta do André = " + contaDoAndre.saldo);
 
-ContaCorrente contaDaMaria = new ContaCorrente();
-contaDaMaria.titular = "Maria Souza";
-contaDaMaria.numero_agencia = 17;
-contaDaMaria.conta = "1010-5";
-contaDaMaria.saldo = 350;
+ContaCorrente contaDoAndre2 = new ContaCorrente();
+contaDoAndre2.titular = "André Silva";
+contaDoAndre2.numero_agencia = 15;
+contaDoAndre2.conta = "1010-X";
+contaDoAndre2.saldo = 100;
+Console.WriteLine("Saldo da conta do André = " + contaDoAndre2.saldo);
 
-Console.WriteLine("Saldo da conta d Maria = " + contaDaMaria.saldo);
+Console.WriteLine(contaDoAndre == contaDoAndre2);
 
-contaDoAndre.Transferir(50, contaDaMaria);
-Console.WriteLine("Saldo da conta do andré = " + contaDoAndre.saldo);
-Console.WriteLine("Saldo da conta da Maria = " + contaDaMaria.saldo);
-
-
-
-/*
-Console.WriteLine("Saldo da conta do andré = " + contaDoAndre.saldo);
-
-contaDoAndre.Depositar(100);
-
-Console.WriteLine("Saldo da conta do andré pós deposito = " + contaDoAndre.saldo);
-
-if (contaDoAndre.Sacar(100) == true)
-{
-    Console.WriteLine("Saldo da conta do andré pós saque = " + contaDoAndre.saldo);
-}
-else
-{
-    Console.WriteLine("Saldo insuficiente");
-}*/
 
